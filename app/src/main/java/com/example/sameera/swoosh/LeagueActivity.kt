adadc7dcfe37a49d60d56bf54adf957f9cfe4b87
Sameera Dissanayaka <sameerad177@gmail.com>
2019-01-29 14:08:17 +0530

    Skill Activity Page

--- app/src/main/java/com/example/sameera/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/sameera/swoosh/LeagueActivity.kt
@@ -1,12 +1,19 @@
 package com.example.sameera.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+//Insted of using onCreate we are using onClick Method
+    fun leagueNextClicked(view:View){
+        val skilActivity=Intent(this,SkillActivity::class.java)
+        startActivity(skilActivity)
     }
 }

e1fa2d6116d99bedf00b9321e1a13fe84c36dda6
Sameera Dissanayaka <sameerad177@gmail.com>
2019-01-29 18:43:12 +0530

    Pass one activity data to another activity

---
+++ app/src/main/java/com/example/sameera/swoosh/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.sameera.swoosh
+
+//Globally accessible variable
+//Constant imutable variable EXTRA_LEAGUE
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/com/example/sameera/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/sameera/swoosh/LeagueActivity.kt
@@ -4,16 +4,44 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
-//Insted of using onCreate we are using onClick Method
-    fun leagueNextClicked(view:View){
-        val skilActivity=Intent(this,SkillActivity::class.java)
-        startActivity(skilActivity)
+
+    fun onMensClicked(view: View) {
+        womensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+        selectedLeague = "mens"
+    }
+
+    fun onWomensClicked(view: View) {
+        mensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+        selectedLeague = "womens"
+    }
+
+    fun onCoedClicked(view: View) {
+        mensLeagueBtn.isChecked = false
+        womensLeagueBtn.isChecked = false
+        selectedLeague = "co-ed"
+    }
+
+    //Insted of using onCreate we are using onClick Method
+    fun leagueNextClicked(view: View) {
+        if (selectedLeague != "") {
+            val skilActivity = Intent(this, SkillActivity::class.java)
+            //use constant to use selected league value so use put extra EXTRA_LEAGUE is constant
+            skilActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skilActivity)
+        } else {
+            Toast.makeText(this, "Please Select a League .", Toast.LENGTH_SHORT).show()
+        }
     }
 }
